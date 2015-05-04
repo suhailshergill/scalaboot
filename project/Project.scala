2e0a639d80225d78d2ff22a9ce2d2137c3ca8d38
Suhail Shergill <suhailshergill@gmail.com>
2015-05-03 23:36:29 -0400

    sbt: bump up to scala 2.11.6

--- project/Project.scala
+++ project/Project.scala
@@ -41,7 +41,7 @@ object ScalabootBuild extends Build {
       publishMavenStyle := true,
       organization := ORG_NAME,
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.11.5"
+      scalaVersion := "2.11.6"
     )
 
   def defaultProject: Project => Project = _.
